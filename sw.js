8f72451867f7240b1c9d8022952910c1b1b46a56
aqilhamizan <aqilhamizan2@gmail.com>
2026-07-20 20:13:22 +0800

    fix: close modal div properly so Registered Students table card is fully visible

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@
 // Ini membolehkan app buka SERTA-MERTA walaupun tiada atau lambat internet.
 // ==========================================================================
 
-const CACHE_NAME    = 'upli-static-v9';
-const CACHE_VERSION = 9; // Tingkatkan nombor ini jika ada perubahan fail utama
+const CACHE_NAME    = 'upli-static-v10';
+const CACHE_VERSION = 10; // Tingkatkan nombor ini jika ada perubahan fail utama
 
 // Senarai fail statik yang akan dicache semasa pemasangan
 const STATIC_ASSETS = [
